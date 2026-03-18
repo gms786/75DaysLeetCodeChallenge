c7b2f53c17b2eb3c8a833bf291c17d95e468c48d
Gaushmohammad  Siddiqui <62978478+gms786@users.noreply.github.com>
2026-03-18 16:54:58 +0530

    Time: 5 ms (76.7%), Space: 71.8 MB (34.54%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function(nums) {
+    let n = nums.length;
+    let answer = new Array(n).fill(1);
+
+    // Step 1: Prefix product
+    let prefix = 1;
+    for (let i = 0; i < n; i++) {
+        answer[i] = prefix;
+        prefix *= nums[i];
+    }
+
+    // Step 2: Suffix product
+    let suffix = 1;
+    for (let i = n - 1; i >= 0; i--) {
+        answer[i] *= suffix;
+        suffix *= nums[i];
+    }
+
+    return answer;
+};
