c7746fb3c1f8e5babcb5d3ac90356b2ffa39ffd5
Gaushmohammad  Siddiqui <62978478+gms786@users.noreply.github.com>
2026-03-31 18:24:33 +0530

    Time: 8 ms (79.36%), Space: 64.2 MB (93.08%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.js
@@ -0,0 +1,43 @@
+var MinStack = function () {
+    this.stack = [];
+    this.minStack = [];
+};
+
+/** 
+ * @param {number} val
+ * @return {void}
+ */
+MinStack.prototype.push = function (val) {
+    this.stack.push(val);
+
+    // If minStack is empty OR val is smaller/equal, push it
+    if (this.minStack.length === 0 || val <= this.getMin()) {
+        this.minStack.push(val);
+    }
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function () {
+    let removed = this.stack.pop();
+
+    // If removed element is the current min, remove from minStack also
+    if (removed === this.getMin()) {
+        this.minStack.pop();
+    }
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function () {
+    return this.stack[this.stack.length - 1];
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function () {
+    return this.minStack[this.minStack.length - 1];
+};
