307b9cad4d4521c9bc41b6ef14ed7396cbe299ea
Gaushmohammad  Siddiqui <62978478+gms786@users.noreply.github.com>
2026-04-14 13:46:07 +0530

    Time: 0 ms (100%), Space: 53.6 MB (82.01%) - LeetHub

---
+++ 0226-invert-binary-tree/0226-invert-binary-tree.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function(root) {
+    // Base case
+    if (root === null) return null;
+
+    // Swap left and right
+    let temp = root.left;
+    root.left = root.right;
+    root.right = temp;
+
+    // Recursively invert subtrees
+    invertTree(root.left);
+    invertTree(root.right);
+
+    return root;
+};
