437ecd58d8f1b54e6317008b44292da4c6ec17d3
Gaushmohammad  Siddiqui <62978478+gms786@users.noreply.github.com>
2026-06-02 13:12:54 +0530

    Time: 0 ms (100%), Space: 53.9 MB (92.03%) - LeetHub

---
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} cost
+ * @return {number}
+ */
+var minCostClimbingStairs = function(cost) {
+    let prev2 = cost[0];
+    let prev1 = cost[1];
+
+    for (let i = 2; i < cost.length; i++) {
+        let curr = cost[i] + Math.min(prev1, prev2);
+        prev2 = prev1;
+        prev1 = curr;
+    }
+
+    return Math.min(prev1, prev2);
+};
